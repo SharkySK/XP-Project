f110cc3af593df878225c0b0e8a64cc18b12396d
OnkelDunkel <rasbalnor@gmail.com>
2017-09-20 17:41:49 +0200

    added CustomerController and gitignore

---
+++ src/CustomerController.java
@@ -0,0 +1,36 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+/**
+ * Created by Rasmus on 20-09-2017.
+ */
+public class CustomerController
+{
+    @FXML
+    private TableView activityTable;
+    @FXML
+    private DatePicker dateField;
+    @FXML
+    private ChoiceBox startTimeField;
+    @FXML
+    private ChoiceBox endTimeField;
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private TextField phoneNoField;
+    @FXML
+    private TextField participantsField;
+    @FXML
+    private TextField priceField;
+    @FXML
+    private TextField ageField;
+    @FXML
+    private TextField heightField;
+    @FXML
+    private TextField instructorField;
+}
