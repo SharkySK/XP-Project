9be702a6e26497b45d7cba14f63c560ebfe541cb
jakub <jaku0584@stud.kea.dk>
2017-09-26 11:52:01 +0200

    Stats should be working

--- src/StatsController.java
+++ src/StatsController.java
@@ -1,6 +1,8 @@
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableView;
@@ -22,23 +24,35 @@ public class StatsController {
     @FXML
     public TableView tableView;
 
+    public DBConn dbConn = new DBConn();
+    private ObservableList<Booking> statsList = FXCollections.observableArrayList();
+
     @FXML
     public void initialize(){
         choiceBox.setItems(FXCollections.observableArrayList("Day", "Week", "Month"));
     }
     @FXML
     public void showStats(ActionEvent e){
-        statsDate();
+        loadStatsTable(statsDate());
     }
 
-    public LocalDate statsDate(){
+    public void loadStatsTable(ArrayList<Booking> b){
+        statsList = FXCollections.observableArrayList(b);
+        tableView.setItems(statsList);
+    }
+
+    public ArrayList statsDate(){
+
         LocalDate date = datePicker.getValue();
 
         if(choiceBox.getSelectionModel().isEmpty() || choiceBox.getValue().equals(null)){
             JOptionPane.showMessageDialog(null,"Please choose a date or period first!");
             return null;
         } else if(choiceBox.getValue().equals("Day")){
-            return date;
+            System.out.println("Day selected");
+
+            return dbConn.getBookingsByDates(singleDay(date).get(0),singleDay(date).get(0));
+
         } else if(choiceBox.getValue().equals("Week")){
             int wN = weekNum(date);
             //Monday of week;
@@ -54,14 +68,30 @@ public class StatsController {
             Date input2 = cal.getTime();
             LocalDate weekEnd = input2.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
 
-            weekStartEnd(weekStart,weekEnd);
+            System.out.println("Week selected");
+
+            return dbConn.getBookingsByDates(weekStart,weekEnd);
 
         } else if(choiceBox.getValue().equals("Month")){
-            
-            return date;
-        }
 
-        return date;
+            LocalDate firstDayMonth = date.withDayOfMonth(1);
+            LocalDate lastDayMonth = date.withDayOfMonth(date.lengthOfMonth());
+
+
+            System.out.println("Month selected");
+
+            return dbConn.getBookingsByDates(firstDayMonth,lastDayMonth);
+
+        }
+        return null;
+    }
+
+    public static List<LocalDate> singleDay(LocalDate dayDate){
+
+        List<LocalDate> result = new ArrayList<LocalDate>();
+        result.add(dayDate);
+
+        return result;
     }
 
     public int weekNum(LocalDate date){
@@ -72,14 +102,14 @@ public class StatsController {
         return weekOfYear;
     }
 
-    public static List<LocalDate> weekStartEnd(LocalDate startDate, LocalDate endDate){
-
-        List<LocalDate> result = new ArrayList<LocalDate>();
-        result.add(startDate);
-        result.add(endDate);
-
-        return result;
-    }
+//    public static List<LocalDate> weekStartEnd(LocalDate startDate, LocalDate endDate){
+//
+//        List<LocalDate> result = new ArrayList<LocalDate>();
+//        result.add(startDate);
+//        result.add(endDate);
+//
+//        return result;
+//    }
 
 
 }
