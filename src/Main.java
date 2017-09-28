50b0d9a9bba4ade5cebc5c737e7c03dd829a674c
Rahzv <razv0104@stud.kea.dk>
2017-09-28 10:38:32 +0200

    Revert "Shit CSS best colors 2k17"
    This reverts commit 6314ff541e19bd2306e746db44c21d641890163a.

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,6 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("GUI/LoginScreen.fxml"));
         primaryStage.setTitle("Booking system");
         Scene scene = new Scene(root,551,355);
-        scene.getStylesheets().addAll(this.getClass().getResource("xpStyleSheet.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.show();
     }
